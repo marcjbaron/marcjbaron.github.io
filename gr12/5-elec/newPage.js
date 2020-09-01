53b37b3d769f814b5df3f9971149f7eb64c7f5e3
Marc BARON <marcjbaron@gmail.com>
2020-09-01 13:42:26 -0400

    testing nextpage

---
+++ gr12/5-elec/newPage.js
@@ -0,0 +1,11 @@
+let clicks = 1;
+$(document).ready(function(){
+  $("button").next.click(function(){
+  clicks++;
+  $("#nextpage").load('pg'+ clicks + '.html');
+
+});
+  $("button").previous.click(function(){
+  clicks--;
+  history.back();
+};
